82300e3cabb011471ba8bcf6f5ad8b3e425a0180
Sowmya27904 <sowmyamallipati@gmail.com>
2025-06-15 10:31:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSection(sectionId) {
+  const sections = document.querySelectorAll('.content-section');
+  sections.forEach((section) => {
+    section.classList.add('hidden');
+  });
+
+  document.getElementById(sectionId).classList.remove('hidden');
+}
